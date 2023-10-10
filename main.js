e3dbf260b79796a0aeb380856966a3c36b076b70
madu.vtr <110983547+maduvalter@users.noreply.github.com>
2023-10-10 11:24:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Pom  ---------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+ const  listaDeTeclas = document.querySelectorAll('.tecla');
